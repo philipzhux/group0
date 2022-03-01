e3389976318dd8027712d9cf38c07c797cf4647c
aditya <aditya09@berkeley.edu>
2022-03-01 18:51:26 +0000

    modified seek-and-tell.c

--- src/tests/userprog/seek-and-tell.c
+++ src/tests/userprog/seek-and-tell.c
@@ -5,20 +5,41 @@
 #include <string.h>
 
 void test_main(void) {
-    int fd = open("sample.txt");
-    if (fd < 2) {
-        fail("open() returned %d", fd);
+    int fd1 = open("sample.txt");
+    int fd2 = open("sample.txt");
+    
+    if (fd1 < 2) {
+        fail("open() returned fd1: %d", fd1);
     }
+
+    if (fd2 < 2) {
+        fail("open() returned fd2: %d", fd2);
+    }
+
     int BUFF_SIZE = 15;
-    char buffer[BUFF_SIZE + 1];
-    buffer[BUFF_SIZE] = '\n';
-    seek(fd, 9);
-    if (tell(fd) != 9) {
-        msg("tell returned %d", tell(fd));
+    char buff1[BUFF_SIZE + 1];
+    char buff2[BUFF_SIZE + 1];
+    buff1[BUFF_SIZE] = '\n';
+    buff2[BUFF_SIZE] = '\n';
+
+    seek(fd1, 9);
+    if (tell(fd1) != 9) {
+        msg("tell on fd1 returned %d", tell(fd1));
     }
-    int bytes_read = read(fd, buffer, BUFF_SIZE);
+    int bytes_read = read(fd1, buff1, BUFF_SIZE);
     if (bytes_read != BUFF_SIZE) {
-        msg("read() only read %d bytes", bytes_read);
+        msg("read() on fd1 only read %d bytes", bytes_read);
     }
-    write(1, buffer, BUFF_SIZE + 1);
+    
+    bytes_read = read(fd2, buff2, BUFF_SIZE);
+    if (bytes_read != BUFF_SIZE) {
+        msg("read() on fd2 only read %d bytes", bytes_read);
+    }
+    char * s = "buff: ";
+    write(1, s, strlen(s));
+    write(1, buff1, BUFF_SIZE + 1);
+
+    write(1, s, strlen(s));
+    write(1, buff2, BUFF_SIZE + 1);
+
 }
--- src/tests/userprog/seek-and-tell.ck
+++ src/tests/userprog/seek-and-tell.ck
@@ -4,7 +4,8 @@ use warnings;
 use tests::tests;
 check_expected (IGNORE_USER_FAULTS => 1, [<<'EOF']);
 (seek-and-tell) begin
-Electronic Fact
+buff: Electronic Fact
+buff: "Amazing Electr
 (seek-and-tell) end
 seek-and-tell: exit(0)
 EOF
